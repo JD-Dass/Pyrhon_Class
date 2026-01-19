38165ddab9e7ba47b3c47a389d2b9c39c336bd24
JD-Dass <jd9360dass@gmail.com>
2026-01-19 14:17:20 +0530

    modules

---
+++ Class18_moduls.py
@@ -0,0 +1,117 @@
+"""
+# Built in modules in python
+built in modules are modules that come pre-installed with python.
+you don't need to sownload or install them seperately. You can directly use them using the import keyword.
+these modules helps you do common tasks like math oprrations , date and time handling , random numbers , system interaction , file handling , etc.
+
+# Why built in modules are important? 
+save time (no need to write everything from scrach)
+reliable and optimized
+used in real projects and interviews
+help write clean and short code
+
+How to use a built in module?
+example:
+import module_name
+
+# Commonly used built in modules:
+=> math module
+=> random module
+=> datetime module
+=> time module
+=> os module
+=> sys module
+=> calender module
+=> statistics module
+=> json module
+=> re module
+
+# math module
+the math module is a built in python module used to perform mathematical operations
+is provides ready made functions for square root, power, trigonometry, logarithms, constants, etc
+# you don't neet to install anything just import math
+
+how to use math module?
+import math
+
+# commonly used functions in math module
+1. sqrt() => Square root
+2. pow() => power
+3. ceil() => round up
+4. floor() => round down
+5. fabs() => Absolute value
+6. factorial() => factorial
+7. trignometric functions
+=> sin()
+=> cos()
+=> tan
+8. logarithmic function
+=> log()
+=> log10()
+9. constants in math module
+=> math.pi
+=> math.e
+
+real world use of math modules
+=>calculator
+=>Scientific applications
+=> game physics
+=> data analysis
+=> engineering programs
+
+import math
+
+# sqrt() – Square Root
+a = 10.53328
+b = 3
+c =10
+print(math.sqrt(a))
+
+# pow() – Power
+print(math.pow(a, b))
+
+# ceil() – Round UP
+print(math.ceil(a))
+
+# floor() – Round DOWN
+print(math.floor(a))
+
+# fabs() – Absolute Value
+print(math.fabs(a))
+
+# factorial() – Factorial
+print(math.factorial(c))
+
+# Trigonometric Functions
+print(math.sin(math.pi / a))
+print(math.cos(a))
+print(math.tan(a))
+
+# Logarithmic function
+print(math.log(a))
+print(math.log10(a))
+
+# constrants in math module
+print(math.pi)
+print(math.e)
+
+
+
+# random module
+the random module is a built in pyhton module used to genarate random numbers and randome selections
+it is commonly used in games, otp ganaration, simulations, quizzes, and testing.
+# no install needed just import random
+
+# hou to use random module?
+import random
+
+commonly used functions is random module
+1. random() => random float (0.0 to 1.0)
+2. randint() => random integer(inclusive)
+3. randrange() => random number in range
+4. choice() => random element from sequence
+5. choices() => Multiple random Elements
+6. shuffle() => shuffle list
+7. sample() => random unique elements
+
+"""
